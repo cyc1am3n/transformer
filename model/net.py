e4c4661ad1e326891ec66f4b5404ec960ccffd41
Daeyoung Kim <kantori321@korea.ac.kr>
2020-11-24 16:10:22 +0900

    (feat) Transformer Decoder
    - fix some errors in Encoder
    - edit data configuration
    - DecoderLayer (model.ops)
    - Decoder (model.net)
    - Transformer (model.net)

--- model/net.py
+++ model/net.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
-from torch.nn import Functional as F
+import torch.nn.functional as F
 
-from ops import *
+from .ops import *
 
 class Encoder(nn.Module):
     def __init__(self, data_config, model_config):
+        super().__init__()
         self.n_layers = model_config['n_layers']
         self.d_model = model_config['d_model']
         self.n_heads = model_config['n_heads']
@@ -14,9 +15,9 @@ class Encoder(nn.Module):
         self.d_ff = model_config['d_ff']
         self.max_len = model_config['max_len']
         self.dropout = model_config['dropout']
-        self.n_vocab = data_config['vocab_size']
+        self.n_vocab = data_config['spm']['vocab_size']
 
-        self.embedding = Embedding(self.n_vocab, self.d_model, self.max_len, self.dropout)
+        self.embedding = EmbeddingLayer(self.n_vocab, self.d_model, self.max_len, self.dropout)
         modules = [EncoderLayer(self.d_model, self.n_heads, self.d_k, self.d_v, self.d_ff, self.dropout) for _ in range(self.n_layers)]
         self.sequential = nn.Sequential(*modules)
 
@@ -24,3 +25,42 @@ class Encoder(nn.Module):
         x = self.embedding(x)
         out = self.sequential(x)
         return out
+
+class Decoder(nn.Module):
+    def __init__(self, data_config, model_config):
+        super().__init__()
+        self.n_layers = model_config['n_layers']
+        self.d_model = model_config['d_model']
+        self.n_heads = model_config['n_heads']
+        self.d_k = model_config['d_k']
+        self.d_v = model_config['d_v']
+        self.d_ff = model_config['d_ff']
+        self.max_len = model_config['max_len']
+        self.dropout = model_config['dropout']
+        self.n_vocab = data_config['spm']['vocab_size']
+
+        self.embedding = EmbeddingLayer(self.n_vocab, self.d_model, self.max_len, self.dropout)
+        self.decoder_layers = nn.ModuleList([DecoderLayer(self.d_model, self.n_heads, self.d_k, self.d_v, self.d_ff, self.dropout) for _ in range(self.n_layers)])
+
+        self.dropout = nn.Dropout(p=self.dropout)
+        self.linear = nn.Linear(self.d_model, self.n_vocab)
+        self.softmax = nn.Softmax(dim=2)
+
+    def forward(self, enc_out, x):
+        x = self.embedding(x)
+        for i in range(self.n_layers):
+            x = self.decoder_layers[i](enc_out, x)
+        x = self.linear(self.dropout(x))
+        out = self.softmax(x)
+        return out
+
+class Transformer(nn.Module):
+    def __init__(self, data_config, model_config):
+        super().__init__()
+        self.encoder = Encoder(data_config, model_config)
+        self.decoder = Decoder(data_config, model_config)
+
+    def forward(self, src, tgt):
+        enc_out = self.encoder(src)
+        prob = self.decoder(enc_out, tgt)
+        return tgt
--- model/ops.py
+++ model/ops.py
@@ -2,18 +2,22 @@ import math
 
 import torch
 import torch.nn as nn
-from torch.nn import Functional as F
+import torch.nn.functional as F
 
 class PositionalEncodingLayer(nn.Module):
-    def __init__(self, d_model, max_len, dropout=0.1):
+    def __init__(self, embed_dim, max_len, dropout=0.1):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
         position = torch.arange(0, max_len, dtype=torch.float).reshape(-1, 1)
-        div = torch.pow(10000, torch.arange(0, d_model, 2, dtype=torch.float) / d_model)
-        self.pe = torch.zeros(max_len, d_model)
-        self.pe[:, 0::2] = torch.sin(position / div)
-        self.pe[:, 1::2] = torch.cos(position / div)
+        div = torch.pow(10000, torch.arange(0, embed_dim, 2, dtype=torch.float) / embed_dim)
         
+        pe = torch.zeros(max_len, embed_dim)
+        if torch.cuda.is_available():
+            pe = pe.cuda()
+        pe[:, 0::2] = torch.sin(position / div)
+        pe[:, 1::2] = torch.cos(position / div)
+        self.pe = pe.unsqueeze(0)
+
     def forward(self, x):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
@@ -24,47 +28,49 @@ class EmbeddingLayer(nn.Module):
         self.n_vocab = n_vocab
         self.embed_dim = embed_dim
         self.embedding = nn.Embedding(n_vocab, embed_dim)
-        self.pe = PositionalEncodingLayer(d_model, max_len, dropout)
+        self.pe = PositionalEncodingLayer(embed_dim, max_len, dropout)
     
     def forward(self, x):
         x = self.embedding(x)
         return self.pe(x)
 
 class ScaledDotProductAttentionLayer(nn.Module):
-    def __init__(self, d_model, d_k, d_v, mask=None):
+    def __init__(self, d_model, d_k, d_v, is_mask=False):
         super().__init__()
-        self.mask = mask
+        self.is_mask = is_mask
+        self.d_k = d_k
         self.q_linear = nn.Linear(d_model, d_k)
         self.k_linear = nn.Linear(d_model, d_k)
         self.v_linear = nn.Linear(d_model, d_v)
         
-    def forward(self, x):
-        q = q_linear(x) # N x L x d_k
-        k = k_linear(x) # N x L x d_k
-        v = v_linear(x) # N x L x d_v
-        scores = torch.matmul(q, k.transpose(1, 2)) / math.sqrt(d_k) # N x L x L
+    def forward(self, q_input, k_input, v_input):
+        q = self.q_linear(q_input) # N x L x d_k
+        k = self.k_linear(k_input) # N x L x d_k
+        v = self.v_linear(v_input) # N x L x d_v
+        scores = torch.matmul(q, k.transpose(1, 2)) / math.sqrt(self.d_k) # N x L x L
         
-        if mask is not None:
-            mask = mask.reshape(-1, 1)
-            scores = scores.masked_fill(mask==0, 0)
+        if self.is_mask is not None:
+            max_len = q_input.shape[1]
+            mask = torch.tril(torch.ones(max_len, max_len), diagonal=0).unsqueeze(0) # 1 x L x L
+            if torch.cuda.is_available():
+                mask = mask.cuda()
+            scores = scores.masked_fill(mask==0, -1.0e9)
             
         scores = F.softmax(scores, dim=2) # N x L x L
-        if dropout is not None:
-            scores = dropout(scores)
         output = torch.matmul(scores, v) # N x L x d_v
         return output
 
 class MultiHeadAttentionLayer(nn.Module):
-    def __init__(self, n_heads, d_model, d_k, d_v, mask=None, dropout=0.1):
+    def __init__(self, n_heads, d_model, d_k, d_v, is_mask=False, dropout=0.1):
         super().__init__()
         self.n_heads = n_heads
         self.d_model = d_model
-        self.dropout = nn.Droppout(p=dropout)
-        self.multihead = [ScaledDotProductAttentionLayer(d_model, d_k, d_v, mask) for _ in range(n_heads)]
+        self.dropout = nn.Dropout(p=dropout)
+        self.multihead = nn.ModuleList([ScaledDotProductAttentionLayer(d_model, d_k, d_v, is_mask) for _ in range(n_heads)])
         self.linear = nn.Linear(n_heads * d_v, d_model)
         
-    def forward(self, x):
-        head_outputs = [self.multihead[i](x) for i in range(self.n_heads)]
+    def forward(self, q_input, k_input, v_input):
+        head_outputs = [self.multihead[i](q_input, k_input, v_input) for i in range(self.n_heads)]
         concat = torch.cat(head_outputs, dim=2) # N x L x (d_v * n_heads)
         output = self.linear(concat) # N x L x d_model
         return output
@@ -91,6 +97,21 @@ class EncoderLayer(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
-        x = self.layernorm(x + self.dropout(self.multihead(x)))
+        x = self.layernorm(x + self.dropout(self.multihead(x, x, x)))
+        x = self.layernorm(x + self.dropout(self.ffn(x)))
+        return x
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, n_heads, d_k, d_v, d_ff, dropout=0.1):
+        super().__init__()
+        self.masked_multihead = MultiHeadAttentionLayer(n_heads, d_model, d_k, d_v, is_mask=True, dropout=dropout)
+        self.multihead = MultiHeadAttentionLayer(n_heads, d_model, d_k, d_v, dropout)
+        self.layernorm = nn.LayerNorm(d_model)
+        self.ffn = PositionwiseFFLayer(d_model, d_ff)
+        self.dropout = nn.Dropout(p=dropout)
+    
+    def forward(self, enc_out, x):
+        x = self.layernorm(x + self.dropout(self.multihead(x, x, x)))
+        x = self.layernorm(x + self.dropout(self.masked_multihead(x, enc_out, enc_out)))
         x = self.layernorm(x + self.dropout(self.ffn(x)))
         return x
